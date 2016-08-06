07deaaee210469135b6541c6a9765e85315921b4
Drew Barrett <dfb1998@gmail.com>
2016-08-06 16:24:18 -0400

    added scoreboard

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
     float seconds = 0;
     public GameObject scoreboard;
+    public GameObject scoreHolder;
+    public GameObject scoreText;
 	// Use this for initialization
 	void Start () {
         scoreboard.GetComponent<dreamloLeaderBoard>().LoadScores();
@@ -13,11 +16,31 @@ public class ScoreManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         seconds += Time.deltaTime;
+        if (scoreHolder.activeSelf)
+        {
+            //UpdateScoreboard();
+        }
 	}
 
     public void GameOver()
     {
         scoreboard.GetComponent<dreamloLeaderBoard>().AddScore(System.Environment.UserName, GetComponent<GameManager>().day, Mathf.FloorToInt(seconds), "v0");
-        Debug.Log(scoreboard.GetComponent<dreamloLeaderBoard>().ToListHighToLow().ToString());
+        UpdateScoreboard();
+    }
+    void UpdateScoreboard()
+    {
+        var list = scoreboard.GetComponent<dreamloLeaderBoard>().ToListHighToLow();
+        scoreHolder.SetActive(true);
+        foreach (var item in list)
+        {
+            string s = item.playerName + " | " + item.score + " | " + item.seconds + " | " + item.shortText;
+            GameObject text = (GameObject)Instantiate(scoreText, scoreHolder.transform);
+            //text.transform.localScale = Vector3.one;
+            text.GetComponent<Text>().text = s;
+            //text.transform.SetParent(scoreHolder.transform);
+        }
+        GameObject t = (GameObject)Instantiate(scoreText, scoreHolder.transform);
+        //text.transform.localScale = Vector3.one;
+        t.GetComponent<Text>().text = "YOU: " + System.Environment.UserName + " | " + GetComponent<GameManager>().day + " | " + Mathf.FloorToInt(seconds);
     }
 }
